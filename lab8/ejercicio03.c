a9c242196e2d8375578dfccd564f220c1f3e94f4
Diego Alonso Huamani Luque <dhuamanilu@unsa.edu.pe>
2023-11-02 16:40:18 -0500

    lab8

---
+++ lab8/ejercicio03.c
@@ -0,0 +1,19 @@
+#include <pthread.h>
+#include <semaphore.h>
+sem_t mutex;
+void *my_thread(void *arg){
+ while(1){
+ sem_wait(&mutex);
+printf("Entra\n");
+printf("Sale\n");
+ sem_post(&mutex);
+ }
+}
+int main(){
+pthread_t thread1_id, thread2_id;
+sem_init(&mutex, 0, 1);
+pthread_create(&thread1_id, NULL, &my_thread, NULL);
+pthread_create(&thread2_id, NULL, &my_thread, NULL);
+pthread_join(thread1_id, NULL);
+pthread_join(thread2_id, NULL);
+}
---
+++ lab8/ejercicio05.c
@@ -0,0 +1,23 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <stdio.h> // para imprimir
+sem_t mutex;
+void *my_thread(void *arg){
+ while(1){
+ sem_wait(&mutex);
+printf("Entra\n");
+sleep(4);
+printf("Sale\n");
+ sem_post(&mutex);
+ }
+}
+int main(){
+pthread_t thread1_id, thread2_id;
+sem_init(&mutex, 0, 1);
+pthread_create(&thread1_id, NULL, &my_thread, NULL);
+sleep(2);
+pthread_create(&thread2_id, NULL, &my_thread, NULL);
+pthread_join(thread1_id, NULL);
+pthread_join(thread2_id, NULL);
+}
